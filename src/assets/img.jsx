25bab5be9c347b4438aaf7bcd12fd7b7b0ffb02c
HealedMf <bewajigamer20@gmail.com>
2024-09-18 14:07:14 +0100

    Updated info: styles,imgs and new components

---
+++ src/assets/img.jsx
@@ -0,0 +1,23 @@
+import img1 from "../img/Artboard.png"
+import img2 from "../img/herosection.png"
+import img3 from "../img/Airplane.png"
+import img4 from "../img/call.png"
+import img5 from "../img/facebook.png"
+import img6 from "../img/X.png"
+import img7 from "../img/instagram.png"
+import img8 from "../img/mail.png"
+import img9 from "../img/Frame26.png"
+
+
+
+const Images = {
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9 
+};

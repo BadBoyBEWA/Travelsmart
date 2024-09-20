17a155198a1758eb9516e357e8924268680d0d87
HealedMf <bewajigamer20@gmail.com>
2024-09-20 15:11:27 +0100

    little error

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -47,7 +47,6 @@ export default function Aboutus() {
           <Airplane airimg={Images.img14}/>
           </div>
         </div>
-g
         <div className='promise-sec'>
           <div>
           <Text texts='Our Promise' textstyle='promisetx'/>

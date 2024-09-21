2118531fd3db72692189a7ac188b3b32afd20ef2
PRECIOUS FAVOUR DICK <PRECHYCUTIE14@GMAIL.COM>
2024-09-21 02:48:58 +0100

    done omo

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,20 +14,21 @@ import Navbar from './Navbar.jsx';
 
 export default function Home(){
   return (
+    <>
     <div className='whole'>
-<Navbar/>
+      <Navbar/>
 
       <section className='container' id='homepage'>
         <div className='herosec'>
-        <div className='herotx'>
-          <div className='cust-sec'>
-          <Logo logoimg={Images.img11} logosec='custpic'/><Text texts='100k+ customer' textstyle='customer'/>
-          </div>
-          <Text texts="Your Trusted All-In-One" textstyle='herotx1' travel="Travel Companion" travelstyle='herotx2'/>
-          <Text texts="Find and Book Hotels, Flights, Car Rentals, and Unique Experiences Across the Globe –Tailored Recommendations for Every Journey" textstyle='herotx3'/>
+          <div className='herotx'>
+            <div className='cust-sec'>
+              <Logo logoimg={Images.img11} logosec='custpic'/><Text texts='100k+ customer' textstyle='customer'/>
+            </div>
+            <Text texts="Your Trusted All-In-One" textstyle='herotx1' travel="Travel Companion" travelstyle='herotx2'/>
+            <Text texts="Find and Book Hotels, Flights, Car Rentals, and Unique Experiences Across the Globe –Tailored Recommendations for Every Journey" textstyle='herotx3'/>
             <Buttons buttx='Trip Catalog' butstyle='catalog' butlink='catlink' triplink='./tripcatalog'/>
-        </div>
-        <Airplane airimg={Images.img3} airstyle='airplane'/>
+          </div>
+          <Airplane airimg={Images.img3} airstyle='airplane'/>
         </div>
       </section>
 
@@ -38,9 +39,10 @@ export default function Home(){
             <Text texts='Travel Smart is your one-stop destination for all your travel planning needs. Whether you are dreaming of a tropical getaway, a cultural adventure, or a city break, we are here to help you plan the perfect trip' textstyle='abouttx2'/>
             <Buttons buttx='Explore' butstyle='exp' butlink='explink' triplink='./explore'/>
           </div>
-          </div>
+        </div>
       </section>
       <Footers/>
     </div>
+    </>
   )
 };
